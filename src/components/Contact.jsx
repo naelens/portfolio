a96ffb6bbd582ab7d75876d11a31e0f2bdbf6c84
naelem <ellenxxy19@gmail.com>
2025-01-30 13:58:22 -0300

    Adding states to clear form fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,19 @@
 import { BehanceLogo, GithubLogo, LinkedinLogo } from "phosphor-react";
+import { useState } from "react";
+//import { useState } from "react";
 
 export function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    function handleSetForm() {
+        setName("");
+        setEmail("");
+        setMessage("");
+    }
+    
+
     return (
         <div id="contato" className="flex flex-col justify-center items-center font-inter px-8 py-10 sm:px-7.5rem sm:py-20 bg-gray-950">
             <div className="border border-purple-400 rounded-lg shadow-lg shadow-gray-500/20 p-4 sm:p-8 sm:w-40rem">
@@ -9,7 +22,7 @@ export function Contact() {
                 </section>
 
                 <form action="https://getform.io/f/bvrrgxob" method="POST" className="mt-5 sm:mt-10" >
-                    <div className="gap-4 grid grid-cols-2">
+                    <div className="gap-4 grid grid-cols-1 sm:grid-cols-2">
                         <div className="mt-4">
                             <label htmlFor="email" className="block text-xs sm:text-sm 2xl:text-base font-medium text-gray-200">
                                 Nome
@@ -20,6 +33,8 @@ export function Contact() {
                                 name="username"
                                 type="text"
                                 autoComplete="Nome"
+                                onChange={(e) => setName(e.target.value)}
+                                value={name}
                                 className="block w-full rounded-md bg-gray-950 px-3 py-1.5 text-sm 2xl:text-base text-gray-400 outline outline-1 -outline-offset-1 outline-gray-800 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-purple-400"
                                 />
                             </div>
@@ -35,6 +50,8 @@ export function Contact() {
                                 name="email"
                                 type="email"
                                 autoComplete="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
                                 className="block w-full rounded-md bg-gray-950 px-3 py-1.5 text-sm 2xl:text-base text-gray-400 outline outline-1 -outline-offset-1 outline-gray-800 placeholder:text-gray-50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-purple-400"
                                 />
                             </div>
@@ -50,6 +67,8 @@ export function Contact() {
                             id="about"
                             name="about"
                             rows={3}
+                            onChange={(e) => setMessage(e.target.value)}
+                            value={message}
                             className="block resize-none w-full rounded-md bg-gray-950 px-3 py-1.5 text-sm 2xl:text-base text-gray-400 outline outline-1 -outline-offset-1 outline-gray-800 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-purple-400"
                             defaultValue={''}
                             />
@@ -57,7 +76,7 @@ export function Contact() {
                     </div>
 
                     <div className="flex justify-center mt-8">
-                        <button type="submit" className="px-4 py-2 sm:px-6 sm:py-2 rounded-md text-xs sm:text-base font-medium text-gray-50 hover:bg-purple-400 bg-purple-500 hover:text-white transition duration-300 ease-in-out">Enviar</button>
+                        <button onClick={handleSetForm} type="submit" className="px-4 py-2 sm:px-6 sm:py-2 rounded-md text-xs sm:text-base font-medium text-gray-50 hover:bg-purple-400 bg-purple-500 hover:text-white transition duration-300 ease-in-out">Enviar</button>
                     </div>
 
                     <section className="flex flex-col justify-center items-center mt-6 sm:mt-10">
